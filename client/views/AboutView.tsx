e07112ca4dbcaa24e4523e55bb8bb7ddd4c6b537
Robert Steele <robert.wade.steele@gmail.com>
2023-04-09 19:57:50 -0400

    Fixed SSR mismatch (somehow). Added basic routing.

---
+++ client/views/AboutView.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const log = function() {
+    console.log("Hello!")
+}
+
+const AboutView = () => {
+    return (
+        <div>
+            <h1>About View!</h1>
+        </div>
+    )
+}
+
+export default AboutView
